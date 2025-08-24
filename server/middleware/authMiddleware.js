f30b16f035e1cb54c22bddf49e7195a2ea2e2669
Vignesh <collegevignesh28@gmail.com>
2025-08-24 14:51:28 +0530

    auth updated

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -8,7 +8,7 @@ export const protect = async (req, res, next) => {
   if (!userId) {
     res.json({ success: false, message: "not authenticated" });
   } else {
-    const user = await User.findById(userId);
+    const user = await User.findOne({ _id: userId });
     req.user = user;
     next();
   }
