d5e1424fd9c6dbefef59f06164f6193b34d39f3f
Vignesh <collegevignesh28@gmail.com>
2025-08-24 11:59:46 +0530

    _id added

--- server/models/User.js
+++ server/models/User.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    
+    _id: {
+      type: String,
+      required: true,
+    },
     username: {
       type: String,
       required: true,
@@ -23,7 +26,7 @@ const userSchema = new mongoose.Schema(
     recentSearchedCities: [
       {
         type: String,
-       
+        required: true,
       },
     ],
   },
