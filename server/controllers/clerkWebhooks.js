e27ff89557b2084283bae8bffca01096d8493ebb
Vignesh <collegevignesh28@gmail.com>
2025-08-24 14:33:16 +0530

    clerk webhooks updated

--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -21,21 +21,28 @@ const clerkWebhooks = async (req, res) => {
     const { data, type } = req.body;
     console.log("Incoming Clerk webhook:", req.body);
 
-    const userData = {
-      _id: data.id.replace("user_", ""),
-      email: data.email_addresses[0].email_address,
-      username: data.first_name + " " + data.last_name,
-      image: data.image_url,
-    };
+    
 
    
     switch (type) {
       case "user.created": {
+        const userData = {
+      _id: data.id,
+      email: data.email_addresses[0].email_address,
+      username: data.first_name + " " + data.last_name,
+      image: data.image_url,
+    };
         await User.create(userData);
         break;
       }
 
       case "user.updated": {
+        const userData = {
+      _id: data.id,
+      email: data.email_addresses[0].email_address,
+      username: data.first_name + " " + data.last_name,
+      image: data.image_url,
+    };
         await User.findByIdAndUpdate(data.id, userData);
         break;
       }
